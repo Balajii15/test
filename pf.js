cec17c1e459326b2a707c82338cf14654e85b1a0
Balajii15 <balaji2004uu@gmail.com>
2024-05-19 13:32:59 -0700

    balajipf

---
+++ pf.js
@@ -0,0 +1,51 @@
+let top1 = document.getElementById("top1")
+let top2 = document.getElementById("top2")
+let top3 = document.getElementById("top3")
+let top4 = document.getElementById("top4")
+let top5 = document.getElementById("top5")
+let top6 = document.getElementById("top6")
+
+
+let page1 = document.getElementById("page1")
+let page2 = document.getElementById("page2")
+let page3 = document.getElementById("page3")
+let page4 = document.getElementById("page4")
+let page5 = document.getElementById("page5")
+let pagee = document.getElementById("pagee")
+
+
+let backbtn = document.getElementById("back-btn")
+
+function skill(){
+    page1.style.display="block"
+}
+function edu(){
+    page2.style.display="block"
+}
+function interest(){
+    page3.style.display="block"
+}
+function lan(){
+    page4.style.display="block"
+}
+function addl(){
+    page5.style.display="block"
+}
+function rech(){
+    pagee.style.display="block"
+}
+
+function back(){
+    page1.style.display="none"
+    page2.style.display="none"
+    page3.style.display="none"
+    page4.style.display="none"
+    page5.style.display="none"
+    pagee.style.display="none"
+}
+//selecting top 3
+
+function sry(){
+    alert("sorry its under developing it will update soon")
+}
+
